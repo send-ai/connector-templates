dd12028c5b86cb0592d7405c27df7414b7259b56
Daan Vermunt <daanvermunt@gmail.com>
2024-04-15 17:20:33 +0200

    cr

--- templates/export/index.ts
+++ templates/export/index.ts
@@ -3,9 +3,13 @@ import express from "express";
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.post("/", (req, res) => {
   res.send("Hello World!");
+  console.log(JSON.stringify(req.body, null, 2));
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
